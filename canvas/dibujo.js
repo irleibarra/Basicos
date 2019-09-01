7362ae218c29d625a3240c99728854d7076fe7cc
irleibarra <irlenys.ibarra@gmail.com>
2019-09-01 11:22:29 -0400

    dibujito2

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,10 +1,14 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
+dibujarLinea("blue", 10, 200, 300, 10);
 
-lienzo.beginPath(); //siempre se pone al inicio del camino, pone el lapiz
-lienzo.strokeStyle = "red"; //color de la linea
-lienzo.moveTo(100, 100); //inicio
-lienzo.lineTo(200, 200); //fin
-lienzo.stroke(); //esta funcion se encanrga de dibujar la linea
-lienzo.closePath(); //cerrando el camino
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath(); //siempre se pone al inicio del camino, pone el lapiz
+    lienzo.strokeStyle = color; //color de la linea
+    lienzo.moveTo(xinicial, yinicial); //inicio
+    lienzo.lineTo(xfinal, yfinal); //fin
+    lienzo.stroke(); //esta funcion se encanrga de dibujar la linea
+    lienzo.closePath(); //cerrando el camino
+}
