ddde82c3bb2b84d98d7f1971fd48fd47f3fb11cd
irleibarra <irlenys.ibarra@gmail.com>
2019-09-03 19:08:51 -0400

    villa platzi3

--- villa/platzi.js
+++
@@ -1,6 +0,0 @@
-function aleatorio(min, maxi);
-{
-    var resultado;
-    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
-    return resultado;
-}
--- villa_platzi/platzi.js
+++ villa_platzi/platzi.js
@@ -1,1 +1,42 @@
-alert("jayyyyy");
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+var mapa = "tile.png";
+
+var fondo = {
+    url: "tile.png",
+    cargaOK: false
+};
+
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false 
+
+};
+
+var fondo =  new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = "vaca.png";
+vaca.imagen.addEventListener("load", cargarVacas);
+
+
+function cargarFondo()
+{
+
+}
+
+
+function dibujar()
+{
+    papel.drawImage(fondo, 0, 0);
+}
+
+
+function aleatorio(min, maxi)
+{
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
+}
